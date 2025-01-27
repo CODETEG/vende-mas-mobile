e200b83ca6d3a0d731a1587d8983de95476c2c76
Daniel Zhu <dzhu2409@gmail.com>
2025-01-27 14:02:27 -0500

    Add eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,14 @@
-// https://docs.expo.dev/guides/using-eslint/
 module.exports = {
   extends: ['expo', 'prettier'],
   plugins: ['prettier'],
   rules: {
     'prettier/prettier': 'error',
+    '@typescript-eslint/await-thenable': 'error',
+    quotes: ['error', 'single'],
+    'object-shorthand': ['error', 'always'],
+    "@typescript-eslint/no-unnecessary-condition": "error",
+    'no-console': 'warn',
+    '@typescript-eslint/no-explicit-any': 'warn',
   },
   globals: {
     React: 'writable',
